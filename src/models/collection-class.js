5f78823cb796689b7bf63e3ad5fe57d96b4df6f7
bushraAD98 <bushradawdyeh@gmaiil.com>
2022-03-05 18:29:42 +0300

    final

--- src/models/collection-class.js
+++ src/models/collection-class.js
@@ -59,3 +59,46 @@ this.model = model;
 
 
 module.exports = Collection;
+
+// class Collection {
+// constructor(model){
+//     this.model = model
+// }
+// async createRecord(obj){
+//     try {
+//         let newRecord = await this.model.create(obj);
+//         return newRecord
+//     } catch (e) {
+//      console.error('error in creating a new record in model:',this.model.name); 
+//     }
+// }
+
+// async readRecord(id){
+//     try{
+//         if(id){
+//     return await this.model.findOne({where : {id:id}}); }
+//     else{return this.model.fidAll();}
+//     }
+//     catch(e){console.error('error in reading a  record in model:',this.model.name);
+// }
+// }
+
+// async removeRecord(id){
+//     try{
+//         if(id){return await this.model.destroy({where : {id:id}});}
+        
+//     }
+//     catch(e){console.error('error in removing record in model',this.model.name)}
+// }
+
+// async updateRecord(obj,id){
+//     try{
+//         return await this.model.update(obj,{where:{id:id},returning :true})
+//     }
+//     catch(e){console.error('error in updating record in model:',this.model.name)}
+// }
+
+
+// }
+
+// module.exports = Collection;
--- src/routes/clothes.js
+++ src/routes/clothes.js
@@ -1,7 +1,8 @@
 'use strict';
 const express = require ('express');//to make a router
-const {clothes} = require('../models/index')
+// const {clothes} = require('../models/index')
 const routers = express.Router();//to put all the clothes router inside it then export it in the server.js
+const {clothesCollection} = require('../models/index');
 
 //we call the points by the routers to have them all in one place to use them in server.js
 routers.get('/clothes',getAllclothes);
@@ -14,32 +15,31 @@ routers.put('/clothes/:id',updateClothes)
 
 
 async function getAllclothes(req,res){
-    let clothesCollection = await clothes.readRecord(); //findAll a sequelize method
-    res.status(200).json(clothesCollection);
+    let clothes = await clothesCollection.readRecord();//findAll a sequelize method
+    res.status(200).json(clothes);
 }
 
 async function addNewClothes(req,res) {
   let sentClothes = req.body;
-  let newClothes = await clothes.createRecord(sentClothes);//create a sequelize method
+  let newClothes = await clothesCollection.createRecord(sentClothes);//create a sequelize method
   res.status(201).json(newClothes);
 }
 async function getSingleClothes(req,res){
     let customerId = parseInt(req.params.id);//here we use parseInt because the data is returned as a string
-    let theClothes = await clothes.readRecord(customerId);
+    let theClothes = await clothesCollection.readRecord(customerId);
     res.status(200).json(theClothes);
 }
 
 async function removeClothes(req,res){
     let removedID = parseInt(req.params.id);
-    let removedClothes = await clothes.removeRecord(removedID);
+    let removedClothes = await clothesCollection.removeRecord(removedID);
     res.status(204).json(removedClothes);
 }
 
 async function updateClothes(req,res){
  let body =req.body;
  let id = req.params.id;  
- let sentClothes = await clothes.updateRecord(body,id);
-    const updateClothes = await sentClothes.update(body);
+    const updateClothes = await clothesCollection.updateRecord(body,id);
     res.status(201).json(updateClothes);
 }
 
